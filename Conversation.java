9f85e3c5229c5df62bd15db1e1ba0ba6b8749500
alenych <alenych@smith.edu>
2024-02-15 11:39:47 -0500

    k

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,67 @@
+import java.util.Scanner;
+import java.util.Random;
 class Conversation {
 
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    //boolean stillGoing = true;
+
+    // A list of chatbot responses that will be pulled at random
+    String[] chatbot_responses = {"Very interesting",
+                                  "Cool!",
+                                  "Tell me more!",
+                                  "I dont understant that",
+                                  "Can you explain?"};
+    Random rand = new Random();
+  
+    
+    Scanner sc = new Scanner(System.in);
+    
+    // Chatbot introduction
+    System.out.println("\nHello! My name is chatbot!");
+    System.out.println("How many rounds of conversation would you like to have?");
+    int nRound = sc.nextInt();
+    sc.nextLine();
+    
+    // defining a variable transcript and count to keep count of how many lines in the chat
+    String[] transcript = new String[nRound + 2];
+    transcript[0] = "\nCool! What is on your mind today?";
+    System.out.println(transcript[0]);
+    
+    // For loop that runs the chatbot until the user wants to stop
+    for (int count = 0; count < nRound; count++) {
+      String words = sc.nextLine();
+      String computer_response;
+
+
+      String mirrored_r = words.replace("\\bI\\b","you");
+      mirrored_r = mirrored_r.replace("me", "you");
+      mirrored_r = mirrored_r.replace("am", "are");
+      mirrored_r = mirrored_r.replace("my","your");
+      mirrored_r = mirrored_r.replace("your","my");
+      mirrored_r = mirrored_r.replace("you", "I");
+
+
+      
+
+      
+      // Chatbot mirrored version:
+      
+      
+      if (!mirrored_r.equals(words)) {
+        computer_response = mirrored_r;
+        System.out.println(computer_response);
+      }
+      else {
+        computer_response = chatbot_responses[rand.nextInt(chatbot_responses.length)];
+        System.out.println(computer_response);
+      }
+    }
+    System.out.println("Have a good day!");
+    System.out.println(transcript);
+    sc.close();
+    
   }
 }
+  
+
+    
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
-- Which classmates did you consult with while working on this assignment?
-- Which session(s) of TA / office hours did you attend?
+- Which classmates did you consult with while working on this assignment? 
+    I worked with Olivia Kraft
+- Which session(s) of TA / office hours did you attend? 
+    I attended Professor Jordan's office hours on Tuesday from 11 to 12 pm, during the snow day.
 - What are your initial impressions of Java? 
+    Java is very different from python, which I am used to, and the syntax was very difficult for me to get. But I was able to get help from many resources and figure out the differences in coding. Additionally, it was initially hard for me to find how functions that I was used to in python work in java.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+    Most main functions work in the same way as Python there is just different syntax between the two languages, but the overall idea stays the same. Also, I ran into a coding error when I was saying if "My" or "my" in words: but this was an issue similar to one I would come across in Python where I should have said: if "My in words or "my" in words.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    I struggled initially with the random function and also getting the chatbot to run until a certain condition was met, but attending office hours really helped me fix all of my issues. I would recommend future students to start this assignment extra early and attend as many office hours as they can because they truly helped me.
